1c4564073f4fad0cb6708511c1255e1ea39a1b25
Jakub Kaczmarczyk <jakubkaczmarczyk103@gmail.com>
2024-04-12 18:39:46 +0200

    screen do not flip, password encryption

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import sqlite3
+import bcrypt
 
 # Database connection (replace with your database path)
 conn = sqlite3.connect("movie_rental.db")
@@ -9,6 +10,7 @@ cursor = conn.cursor()
 def login_user(username_entry, password_entry, error_label):
     username = username_entry.get()
     password = password_entry.get()
+    password= password.encode('utf-8')
 
     if not username:
         error_label.config(text="Username cannot be empty")
@@ -18,15 +20,19 @@ def login_user(username_entry, password_entry, error_label):
         error_label.config(text="Password cannot be empty")
         return False
 
-    # Check username and password in database
     cursor.execute(
-        f"SELECT * FROM client WHERE username = ? AND password = ?",
-        (username, password)
+        "SELECT * FROM client WHERE username = ?",
+        (username,)
     )
     user = cursor.fetchone()
 
     if user:
-        return True
+        stored_password = user[4]
+        if bcrypt.checkpw(password, stored_password):
+            return True
+        else:
+            error_label.config(text="Invalid username or password")
+            return False
     else:
         error_label.config(text="Invalid username or password")
         return False
@@ -58,8 +64,9 @@ def register_user(username_entry, password_entry, name_entry, surname_entry, err
         error_label.config(text="Username already exists")
         return False
 
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     cursor.execute(
-        f"INSERT INTO client (username, password, name, surname, last_logged_in) VALUES ('{username}', '{password}', '{name}', '{surname}', NULL)"
+        'INSERT INTO client (username, password, name, surname, last_logged_in) VALUES (?,?,?,?,?)', (username, hashed_password, name, surname, None)
     )
     conn.commit()
     username_entry.delete(0, tk.END)
@@ -129,165 +136,136 @@ def get_movies():
 class SimpleGUI(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.width = 1200
-        self.height = 600
         self.title("Movie Rental")
-        self.geometry(f"{self.width}x{self.height}+0+0")
+        self.screen_width = self.winfo_screenwidth()
+        self.screen_height = self.winfo_screenheight()
+
+        self.geometry(f"{self.screen_width}x{self.screen_height}+0+0")
+        self.state('zoomed')
 
         self.login_screen_setup()
         self.register_screen_setup()
         self.my_rents_screen_setup()
         self.find_movie_screen_setup()
         self.main_page_setup()
-        self.main_page_screen.withdraw()
-        self.my_rents_screen.withdraw()
-        self.find_movie_screen.withdraw()
 
         self.switch_to_login()
-        self.withdraw()
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.logged_user = None
 
     ## Screens ##
     def login_screen_setup(self):
-        self.login_screen = tk.Toplevel(self)
-        self.login_screen.title("Movie Rental Login")
-        self.login_screen.geometry(f"{self.width}x{self.height}+0+0")
+        self.title("Movie Rental Login")
 
-        self.login_label_username = tk.Label(self.login_screen, text="Username:")
+        self.login_label_username = tk.Label(self, text="Username:")
         self.login_label_username.pack()
 
-        self.entry_username = tk.Entry(self.login_screen)
+        self.entry_username = tk.Entry()
         self.entry_username.pack()
 
-        self.login_label_password = tk.Label(self.login_screen, text="Password:")
+        self.login_label_password = tk.Label(self, text="Password:")
         self.login_label_password.pack()
 
-        self.entry_password = tk.Entry(self.login_screen, show="*")
+        self.entry_password = tk.Entry(self, show="*")
         self.entry_password.pack()
 
-        self.login_button = tk.Button(self.login_screen, text="Login", command=self.login_user)
+        self.login_button = tk.Button(self, text="Login", command=self.login_user)
         self.login_button.pack()
 
-        self.register_button = tk.Button(self.login_screen, text="Register", command=self.switch_to_register)
+        self.register_button = tk.Button(self, text="Register", command=self.switch_to_register)
         self.register_button.pack()
 
-        self.error_label = tk.Label(self.login_screen, text="")
+        self.error_label = tk.Label(self, text="")
         self.error_label.pack()
-        self.login_screen.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def register_screen_setup(self):
-        self.register_screen = tk.Toplevel(self)
-        self.register_screen.title("Movie Rental Register")
-        self.register_screen.geometry(f"{self.width}x{self.height}+0+0")
 
-        self.register_label_username = tk.Label(self.register_screen, text="Username:")
+        self.title("Movie Rental Register")
+
+        self.register_label_username = tk.Label(self, text="Username:")
         self.register_label_username.pack()
 
-        self.register_entry_username = tk.Entry(self.register_screen)
+        self.register_entry_username = tk.Entry(self)
         self.register_entry_username.pack()
 
-        self.register_label_password = tk.Label(self.register_screen, text="Password:")
+        self.register_label_password = tk.Label(self, text="Password:")
         self.register_label_password.pack()
 
-        self.register_entry_password = tk.Entry(self.register_screen, show="*")
+        self.register_entry_password = tk.Entry(self, show="*")
         self.register_entry_password.pack()
 
-        self.register_label_name = tk.Label(self.register_screen, text="Name:")
+        self.register_label_name = tk.Label(self, text="Name:")
         self.register_label_name.pack()
 
-        self.register_entry_name = tk.Entry(self.register_screen)
+        self.register_entry_name = tk.Entry(self)
         self.register_entry_name.pack()
 
-        self.register_label_surname = tk.Label(self.register_screen, text="Surname:")
+        self.register_label_surname = tk.Label(self, text="Surname:")
         self.register_label_surname.pack()
 
-        self.register_entry_surname = tk.Entry(self.register_screen)
+        self.register_entry_surname = tk.Entry(self)
         self.register_entry_surname.pack()
 
-        self.register_button = tk.Button(self.register_screen, text="Register", command=self.register_user)
+        self.register_button = tk.Button(self, text="Register", command=self.register_user)
         self.register_button.pack()
 
-        self.back_to_login_button = tk.Button(self.register_screen, text="Back to Login", command=self.switch_to_login)
+        self.back_to_login_button = tk.Button(self, text="Back to Login", command=self.switch_to_login)
         self.back_to_login_button.pack()
-        self.register_screen.protocol("WM_DELETE_WINDOW", self.on_closing)
     
     def my_rents_screen_setup(self):
-        self.my_rents_screen = tk.Toplevel(self)
-        self.my_rents_screen.title("My Rents")
-        self.my_rents_screen.geometry(f"{self.width}x{self.height}+0+0")
+        self.title("My Rents")
 
-        self.back_to_main_button = tk.Button(self.my_rents_screen, text="Back to Main Page", command=self.switch_to_main_page)
+        self.back_to_main_button = tk.Button(self, text="Back to Main Page", command=self.switch_to_main_page)
         self.back_to_main_button.pack()
 
-        self.my_rents_label = tk.Label(self.my_rents_screen, text="My Rents")
+        self.my_rents_label = tk.Label(self, text="My Rents")
         self.my_rents_label.pack()
-        self.my_rents_screen.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def find_movie_screen_setup(self):
-        self.find_movie_screen = tk.Toplevel(self)
-        self.find_movie_screen.title("Find Movie")
-        self.find_movie_screen.geometry(f"{self.width}x{self.height}+0+0")
+        self.title("Find Movie")
 
-        self.back_to_main_button = tk.Button(self.find_movie_screen, text="Back to Main Page", command=self.switch_to_main_page)
+        self.back_to_main_button = tk.Button(self, text="Back to Main Page", command=self.switch_to_main_page)
         self.back_to_main_button.pack()
 
-        self.find_movie_label = tk.Label(self.find_movie_screen, text="Find Movie")
+        self.find_movie_label = tk.Label(self, text="Find Movie")
         self.find_movie_label.pack()
-        self.find_movie_screen.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def main_page_setup(self):
-        self.main_page_screen = tk.Toplevel(self)
-        self.main_page_screen.title("Main Page")
-        self.main_page_screen.geometry(f"{self.width}x{self.height}+0+0")
+        self.title("Main Page")
 
-        self.my_rents_button = tk.Button(self.main_page_screen, text="My Rents", command=self.show_my_rents)
+        self.my_rents_button = tk.Button(self, text="My Rents", command=self.switch_to_my_rents)
         self.my_rents_button.pack()
 
-        self.find_movie_button = tk.Button(self.main_page_screen, text="Find Movie", command=self.show_find_movie)
+        self.find_movie_button = tk.Button(self, text="Find Movie", command=self.switch_to_find_movie)
         self.find_movie_button.pack()
-        self.main_page_screen.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     ## Switches ##
     def switch_to_register(self):
         self.close_all_windows()
-        self.register_screen.deiconify()
+        self.register_screen_setup()
 
     def switch_to_login(self):
         self.close_all_windows()
-        self.login_screen.deiconify()
+        self.login_screen_setup()
     
-    def show_my_rents(self):
+    def switch_to_my_rents(self):
         self.close_all_windows()
-        self.my_rents_screen.deiconify()
-        for widget in self.my_rents_screen.winfo_children():
-            if widget.cget("text") != "Back to Main Page":
-                widget.destroy()
+        self.my_rents_screen_setup()
         self.fetch_rents()
 
-    def show_find_movie(self):
+    def switch_to_find_movie(self):
         self.close_all_windows()
-        self.find_movie_screen.deiconify()
-        for widget in self.find_movie_screen.winfo_children():
-            if widget.cget("text") != "Back to Main Page":
-                widget.destroy()
+        self.find_movie_screen_setup()
         self.fetch_movies()
 
     def switch_to_main_page(self):
         self.close_all_windows()
-        self.main_page_screen.deiconify()
+        self.main_page_setup()
 
     def close_all_windows(self):
-        if self.login_screen.winfo_exists():
-            self.login_screen.withdraw()
-        if self.register_screen.winfo_exists():
-            self.register_screen.withdraw()
-        if self.main_page_screen.winfo_exists():
-            self.main_page_screen.withdraw()
-        if self.find_movie_screen.winfo_exists():
-            self.find_movie_screen.withdraw()
-        if self.my_rents_screen.winfo_exists():
-            self.my_rents_screen.withdraw()
+        for widget in self.winfo_children():
+            widget.destroy()
+
 
     ## Calls ##
     def login_user(self):
@@ -309,8 +287,6 @@ class SimpleGUI(tk.Tk):
 
         result = register_user(username_entry, password_entry, name_entry, surname_entry, error_label)
         if result:
-            # If registration successful, hide the register screen and switch to login screen
-            self.register_screen.withdraw()
             self.switch_to_login()
 
     def fetch_rents(self):
@@ -318,9 +294,9 @@ class SimpleGUI(tk.Tk):
         if reservations:
             for i, reservation in enumerate(reservations):
                 label_text = f"Movie: {reservation['movie_title']} | Start Date: {reservation['start_date']} | End Date: {reservation['end_date']} | Price: {reservation['price']}"
-                tk.Label(self.my_rents_screen, text=label_text).pack()
+                tk.Label(self, text=label_text).pack()
         else:
-            tk.Label(self.my_rents_screen, text="No reservations found.").pack()
+            tk.Label(self, text="No reservations found.").pack()
     
     def fetch_movies(self):
         movies = get_movies()
@@ -329,9 +305,9 @@ class SimpleGUI(tk.Tk):
                 label_text = f"Title: {movie['title']} | Category: {movie['category']} | Year: {movie['year']} | Actors: "
                 actors_text = ", ".join([f"{actor['name']} {actor['surname']}" for actor in movie['actors']])
                 label_text += actors_text
-                tk.Label(self.find_movie_screen, text=label_text).pack()
+                tk.Label(self, text=label_text).pack()
         else:
-            tk.Label(self.find_movie_screen, text="No movies found.").pack()
+            tk.Label(self, text="No movies found.").pack()
 
     # Other
     def on_closing(self):
