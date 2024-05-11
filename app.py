1005c9e2738e5bc5708c63796c4faeb5b25e21ae
Jakub Kaczmarczyk <jakubkaczmarczyk103@gmail.com>
2024-05-11 10:28:59 +0200

    inti DB in app file

--- app.py
+++ app.py
@@ -1,6 +1,287 @@
 import tkinter as tk
 import sqlite3
 import bcrypt
+import json
+import random
+from datetime import datetime, timedelta
+
+
+## Start of Feeding data and creating DB
+def feed_database():
+    ## SIMPLE TABLES ##
+    # Done
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS client (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR,
+    surname VARCHAR,
+    username VARCHAR,
+    password VARCHAR,
+    last_logged_in TIMESTAMP
+    )
+    """
+    )
+
+    # Done
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS director (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR,
+    surname VARCHAR
+    )
+    """
+    )
+
+    # Done
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS actor (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR,
+    surname VARCHAR
+    )
+    """
+    )
+
+    # Done
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS producer (
+    id INTEGER PRIMARY KEY,
+    producer_name VARCHAR
+    )
+    """
+    )
+
+    # Done
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS gener (
+    id INTEGER PRIMARY KEY,
+    gener_name VARCHAR
+    )
+    """
+    )
+
+    ## Advanced Tables ##
+    # Done
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS movie (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR,
+    year TIMESTAMP,
+    producer_id INTEGER,
+    director_id INTEGER,
+    count INTEGER,
+    FOREIGN KEY (producer_id) REFERENCES producers(id),
+    FOREIGN KEY (director_id) REFERENCES directors(id)
+    )
+    """
+    )
+
+    # Done
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS rent (
+    id INTEGER PRIMARY KEY,
+    client_id INTEGER,
+    movie_id INTEGER,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    price INTEGER,
+    FOREIGN KEY (client_id) REFERENCES client(id),
+    FOREIGN KEY (movie_id) REFERENCES movies(id)
+    )
+    """
+    )
+
+    ## Join Tables ##
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS movie_actor (
+    actor_id INTEGER,
+    movie_id INTEGER,
+    FOREIGN KEY (actor_id) REFERENCES actors(id),
+    FOREIGN KEY (movie_id) REFERENCES movies(id)
+    )
+    """
+    )
+
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS movie_gener (
+    gener_id INTEGER,
+    movie_id INTEGER,
+    FOREIGN KEY (gener_id) REFERENCES gener(id),
+    FOREIGN KEY (movie_id) REFERENCES movies(id)
+    )
+    """
+    )
+
+
+    # Randoms
+    def get_random_director_id():
+        return random.randint(1, 50)  # Assuming director IDs range from 1 to 64
+
+
+    # Function to get a random producer ID
+    def get_random_producer_id():
+        return random.randint(1, 100)
+
+
+    def random_time():
+        return timedelta(hours=random.randint(0, 23), minutes=random.randint(0, 59))
+
+
+    def add_random_time(start_date_str, end_date_str):
+        # Parse start and end dates
+        start_date = datetime.strptime(start_date_str, "%m/%d/%Y")
+        end_date = datetime.strptime(end_date_str, "%m/%d/%Y")
+
+        # Generate random hour and minute
+        random_hour = random.randint(0, 23)
+        random_minute = random.randint(0, 59)
+
+        # Add random time to start and end dates
+        start_date += timedelta(hours=random_hour, minutes=random_minute)
+        end_date += timedelta(hours=random_hour, minutes=random_minute)
+
+        return start_date, end_date
+
+
+    with open("raw_data.json", "r") as f:
+        data = json.load(f)
+        for client in data["client"]:
+            hashed_password = bcrypt.hashpw(b"1234", bcrypt.gensalt())
+            cursor.execute(
+                """
+            INSERT INTO client (name, surname, username, password)
+            VALUES (?, ?, ?, ?)
+        """,
+                (
+                    client["name"],
+                    client["surname"],
+                    client["username"],
+                    hashed_password,
+                ),
+            )
+
+        for actor in data["actor"]:
+            cursor.execute(
+                """
+            INSERT INTO actor (name, surname)
+            VALUES (?, ?)
+        """,
+                (actor["name"], actor["surname"]),
+            )
+
+        for director in data["director"]:
+            cursor.execute(
+                """
+            INSERT INTO director (name, surname)
+            VALUES (?, ?)
+        """,
+                (director["name"], director["surname"]),
+            )
+
+        for producer in data["producer"]:
+            cursor.execute(
+                """
+            INSERT INTO producer (producer_name)
+            VALUES (?)
+        """,
+                (producer["producer_name"],),
+            )
+
+        for gener in data["gener"]:
+            cursor.execute(
+                """
+            INSERT INTO gener (gener_name)
+            VALUES (?)
+        """,
+                (gener["gener_name"],),
+            )
+
+        for movie in data["movie"]:
+            title = movie["title"]
+            year = movie["year"]
+            count = movie["count"]
+            director_id = get_random_director_id()
+            producer_id = get_random_producer_id()
+
+            cursor.execute(
+                """
+            INSERT INTO movie (title, year, producer_id, director_id, count)
+            VALUES (?, ?, ?, ?, ?)
+        """,
+                (title, year, producer_id, director_id, count),
+            )
+
+            num_actors = random.randint(3, 8)  # Random number of actors from 3 to 8
+            actor_ids = random.sample(
+                range(1, 101), num_actors
+            )  # Assuming actor IDs range from 1 to 100
+            for actor_id in actor_ids:
+                cursor.execute(
+                    """
+                INSERT INTO movie_actor (actor_id, movie_id)
+                VALUES (?, ?)
+            """,
+                    (actor_id, cursor.lastrowid),
+                )  # cursor.lastrowid gets the ID of the last inserted movie
+
+            # Add random genres to the movie_gener table
+            num_genres = random.randint(1, 3)  # Random number of genres from 1 to 3
+            genre_ids = random.sample(
+                range(1, 18), num_genres
+            )  # Assuming genre IDs range from 1 to 17
+            for genre_id in genre_ids:
+                cursor.execute(
+                    """
+                INSERT INTO movie_gener (gener_id, movie_id)
+                VALUES (?, ?)
+            """,
+                    (genre_id, cursor.lastrowid),
+                )  # cursor.lastrowid gets the ID of the last inserted movie
+
+        # Add rentals to the rent table
+    for rental in data["rent"]:
+        start_time, end_time = add_random_time(rental["start_date"], rental["end_date"])
+        client_id = random.randint(1, 10)
+        movie_id = random.randint(1, 300)
+        days_rented = (end_time - start_time).days
+        price_multiplier = random.randint(1, 5)  # Random multiplier for price
+        price = days_rented * price_multiplier
+
+        cursor.execute(
+            """
+        INSERT INTO rent (client_id, movie_id, start_date, end_date, price)
+        VALUES (?, ?, ?, ?, ?)
+    """,
+            (client_id, movie_id, start_time, end_time, price),
+        )
+
+
+    # Commit the changes and close the connection
+    conn.commit()
+
+
+# Main
+# Connect to the database (or create it if it doesn't exist)
+conn = sqlite3.connect("movie_rental.db", detect_types=sqlite3.PARSE_DECLTYPES)
+
+# Create a cursor object
+cursor = conn.cursor()
+
+table_exists = cursor.fetchone() is not None
+if not table_exists:
+    feed_database()
+conn.close()
+## End of Feeding data and creating DB
+
 
 conn = sqlite3.connect("movie_rental.db")
 cursor = conn.cursor()
@@ -81,7 +362,7 @@ def get_user_reservations(username):
         query = """
       SELECT m.title, r.start_date, r.end_date, r.price
       FROM rents r
-      INNER JOIN movies m ON r.movie_id = m.id
+      INNER JOIN movie m ON r.movie_id = m.id
       INNER JOIN client c ON r.user_id = c.id
       WHERE c.username = ?
     """
@@ -106,11 +387,14 @@ def get_movies():
     try:
         cursor.execute(
             """
-        SELECT m.id, m.title, m.category, m.year, m.producer_id, m.director_id, m.count, 
+        SELECT m.id, m.title, m.year, p.id AS producer_id, p.producer_name, 
+        d.id AS director_id, d.name AS director_name, d.surname AS director_surname, m.count, 
             a.id AS actor_id, a.name AS actor_name, a.surname AS actor_surname
-        FROM movies m
-        LEFT JOIN movies_actors ma ON m.id = ma.movie_id
-        LEFT JOIN actors a ON ma.actor_id = a.id
+        FROM movie m
+        LEFT JOIN movie_actor ma ON m.id = ma.movie_id
+        LEFT JOIN actor a ON ma.actor_id = a.id
+        LEFT JOIN producer p ON m.producer_id = p.id
+        LEFT JOIN director d ON m.director_id = d.id
         ORDER BY m.title
         """
         )
@@ -120,10 +404,12 @@ def get_movies():
             (
                 movie_id,
                 title,
-                category,
                 year,
                 producer_id,
+                producer_name,
                 director_id,
+                director_name,
+                director_surname,
                 count,
                 actor_id,
                 actor_name,
@@ -133,10 +419,12 @@ def get_movies():
                 movies[movie_id] = {
                     "id": movie_id,
                     "title": title,
-                    "category": category,
                     "year": year,
                     "producer_id": producer_id,
+                    "producer_name": producer_name,
                     "director_id": director_id,
+                    "director_name": director_name,
+                    "director_surname": director_surname,
                     "count": count,
                     "actors": [],
                 }
@@ -333,16 +621,26 @@ class SimpleGUI(tk.Tk):
 
     def fetch_movies(self):
         movies = get_movies()
+        listbox = tk.Listbox(self, selectmode=tk.MULTIPLE, width=1000, height=600)
         if movies:
-            for i, movie in enumerate(movies):
-                label_text = f"Title: {movie['title']} | Category: {movie['category']} | Year: {movie['year']} | Actors: "
-                actors_text = ", ".join(
-                    [f"{actor['name']} {actor['surname']}" for actor in movie["actors"]]
-                )
-                label_text += actors_text
-                tk.Label(self, text=label_text).pack()
+            columns = ["Title", "Producer", "Director", "Year", "Actors"]
+            header = "{:<100} | {:<50} | {:<50} | {:<10} | {:<100}".format(*columns)
+            listbox.insert(tk.END, header)
+            listbox.insert(tk.END, "-" * len(header))
+
+            for movie in movies:
+                title = movie['title']
+                year = movie['year']
+                producer = movie['producer_name']
+                director = movie['director_name'] + " " + movie['director_surname']
+                actors = ", ".join([f"{actor['name']} {actor['surname']}" for actor in movie["actors"]])
+                row = "{:<100} | {:<50} | {:<50} | {:<10} | {:<100}".format(title, producer, director, year, actors)
+                listbox.insert(tk.END, row)
         else:
-            tk.Label(self, text="No movies found.").pack()
+            listbox.insert(tk.END, "No movies found.")
+
+        listbox.pack()
+
 
     # Other
     def on_closing(self):
